b9e3975aaa578f33efc929e5010cea09619cc41b
Ac Hsu <charliebookhsu@gmail.com>
2018-04-26 18:42:59 +0800

    add unitest

--- app/src/main/java/demo/aac/bookhsu/com/androidarchitecturecomponentdemo/model/User.kt
+++ app/src/main/java/demo/aac/bookhsu/com/androidarchitecturecomponentdemo/model/User.kt
@@ -1,9 +1,11 @@
 package demo.aac.bookhsu.com.androidarchitecturecomponentdemo.model
 
+import java.io.Serializable
+
 /**
  * Created by bookhsu on 2018/4/25.
  */
-class User {
+class User : Serializable {
     var name : String = "";
     var sex : String = "";
     var age : Int = 0;
--- app/src/test/java/demo/aac/bookhsu/com/androidarchitecturecomponentdemo/ExampleUnitTest.kt
+++ app/src/test/java/demo/aac/bookhsu/com/androidarchitecturecomponentdemo/ExampleUnitTest.kt
@@ -1,8 +1,15 @@
 package demo.aac.bookhsu.com.androidarchitecturecomponentdemo
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockito_kotlin.mock
+import demo.aac.bookhsu.com.androidarchitecturecomponentdemo.model.User
+import demo.aac.bookhsu.com.androidarchitecturecomponentdemo.repo.UserRepository
+import demo.aac.bookhsu.com.androidarchitecturecomponentdemo.viewmodel.UserDetailViewModel
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,33 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    lateinit var mUserDetailViewModel : UserDetailViewModel
+
+    @Before
+    fun setUp() {
+        mUserDetailViewModel = UserDetailViewModel()
+        mUserDetailViewModel.init(UserRepository())
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun test_UserDetailViewModel() {
+        mUserDetailViewModel.getUserRepository().initUser("John", 34);
+        Assert.assertEquals(mUserDetailViewModel.getUserRepository().getUser().value?.name, "John")
+        Assert.assertEquals(mUserDetailViewModel.getUserRepository().getUser().value?.age, 34)
+
+        val mock = mock<User> {
+            on{ }
+        }
+    }
+
 }
